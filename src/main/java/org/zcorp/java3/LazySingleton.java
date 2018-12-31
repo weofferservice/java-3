a5026405ea26fd67a03c7e8ea898ed932851ed08
weofferservice <weofferservice@hotmail.com>
2018-12-31 22:55:49 +0300

    Создание ленивого синглетона по паттерну Double checked locking

---
+++ src/main/java/org/zcorp/java3/LazySingleton.java
@@ -0,0 +1,19 @@
+package org.zcorp.java3;
+
+public class LazySingleton {
+    private static volatile LazySingleton instance;
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private LazySingleton() {
+    }
+}
