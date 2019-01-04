12211b0d233a18335614951054ab30a57968fbc4
weofferservice <weofferservice@hotmail.com>
2019-01-04 14:26:07 +0300

    Переименование переменных (в основном, замена имен счетчиков в циклах на более говорящие)

--- src/main/java/org/zcorp/java3/matrix/MatrixUtil.java
+++ src/main/java/org/zcorp/java3/matrix/MatrixUtil.java
@@ -23,27 +23,27 @@ public class MatrixUtil {
 
         CompletionService<Void> completionService = new ExecutorCompletionService<>(executor);
 
-        for (int j = 0; j < matrixSize; j++) {
-            final int columnNumber = j;
+        for (int column = 0; column < matrixSize; column++) {
+            final int columnNumber = column;
             final int columnB[] = new int[matrixSize];
             for (int k = 0; k < matrixSize; k++) {
-                columnB[k] = matrixB[k][j];
+                columnB[k] = matrixB[k][column];
             }
 
             completionService.submit(() -> {
-                for (int i = 0; i < matrixSize; i++) {
-                    int rowA[] = matrixA[i];
+                for (int row = 0; row < matrixSize; row++) {
+                    int rowA[] = matrixA[row];
                     int sum = 0;
                     for (int k = 0; k < matrixSize; k++) {
                         sum += rowA[k] * columnB[k];
                     }
-                    matrixC[i][columnNumber] = sum;
+                    matrixC[row][columnNumber] = sum;
                 }
                 return null;
             });
         }
 
-        for (int x = 0; x < matrixSize; x++) {
+        for (int task = 0; task < matrixSize; task++) {
             completionService.take();
         }
 
@@ -64,18 +64,18 @@ public class MatrixUtil {
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
         int columnB[] = new int[matrixSize];
-        for (int j = 0; j < matrixSize; j++) {
+        for (int column = 0; column < matrixSize; column++) {
             for (int k = 0; k < matrixSize; k++) {
-                columnB[k] = matrixB[k][j];
+                columnB[k] = matrixB[k][column];
             }
 
-            for (int i = 0; i < matrixSize; i++) {
-                int rowA[] = matrixA[i];
+            for (int row = 0; row < matrixSize; row++) {
+                int rowA[] = matrixA[row];
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
                     sum += rowA[k] * columnB[k];
                 }
-                matrixC[i][j] = sum;
+                matrixC[row][column] = sum;
             }
         }
 
@@ -92,9 +92,9 @@ public class MatrixUtil {
         int[][] matrix = new int[size][size];
         Random rn = new Random();
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = rn.nextInt(10);
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                matrix[row][column] = rn.nextInt(10);
             }
         }
         return matrix;
@@ -111,9 +111,9 @@ public class MatrixUtil {
      */
     public static boolean compare(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                if (matrixA[i][j] != matrixB[i][j]) {
+        for (int row = 0; row < matrixSize; row++) {
+            for (int column = 0; column < matrixSize; column++) {
+                if (matrixA[row][column] != matrixB[row][column]) {
                     return false;
                 }
             }
