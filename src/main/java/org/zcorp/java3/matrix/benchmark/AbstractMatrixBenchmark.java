81357ae624dc553de073d9475af24201fa5ae88f
weofferservice <weofferservice@hotmail.com>
2019-01-05 21:32:19 +0300

    Создание метода main, который позволит запускать JMH benchmark-и из IDEA без необходимости ставить "JMH plugin"

--- src/main/java/org/zcorp/java3/matrix/benchmark/AbstractMatrixBenchmark.java
+++ src/main/java/org/zcorp/java3/matrix/benchmark/AbstractMatrixBenchmark.java
@@ -1,6 +1,11 @@
 package org.zcorp.java3.matrix.benchmark;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 import org.zcorp.java3.matrix.MatrixUtil;
 
 import java.util.concurrent.TimeUnit;
@@ -24,6 +29,21 @@ public abstract class AbstractMatrixBenchmark {
     public void matricesSetup() {
         matrixA = MatrixUtil.create(MATRIX_SIZE);
         matrixB = MatrixUtil.create(MATRIX_SIZE);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(MatrixBenchmarkWithExecutorService.class.getSimpleName())
+                .include(MatrixBenchmarkWithoutExecutorService.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .mode(Mode.SingleShotTime)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .threads(1)
+                .forks(1)
+                .timeout(TimeValue.minutes(5))
+                .build();
+        new Runner(options).run();
     }
 
 }
